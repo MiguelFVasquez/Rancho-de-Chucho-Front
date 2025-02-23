bc731b047b7cc8718005a08371c996144272798d
Miguel Florez <florezvasquezjuanmiguel@gmail.com>
2025-02-22 23:45:28 -0500

    Reports

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent {
+  generateHTMLReport() {
+    console.log('📄 Generando reporte en HTML...');
+    // Aquí iría la lógica para generar el reporte en HTML
+  }
 
+  generatePDFReport() {
+    console.log('📄 Generando reporte en PDF...');
+    // Aquí iría la lógica para generar el reporte en PDF
+  }
 }
