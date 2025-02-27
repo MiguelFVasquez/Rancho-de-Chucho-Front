e2ff5f48e8de25dadabb6953555e7ffa5dee04d9
Miguel Florez <florezvasquezjuanmiguel@gmail.com>
2025-02-26 23:07:46 -0500

    Add, edit and delete modal

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {  FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InventoryTableComponent } from '../inventory-table/inventory-table.component';
+import { editProduct } from '../../dto/product/editProductDto';
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [CommonModule, FormsModule, InventoryTableComponent],
+  imports: [CommonModule, FormsModule, InventoryTableComponent,ReactiveFormsModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
 export class InventoryComponent {
   searchTerm: string = '';
   selectedProduct: any = null;
+  productForm: FormGroup;
+  showModal: boolean = false;  
+  showEditModal: boolean = false;
+  showDeleteModal: boolean = false; 
+  editProductData: editProduct = {
+    name: '',
+    marca: '',
+    precio_compra: 0,
+    unidad_medida: ''
+  };
 
   products = [
     { id: 1, name: 'Harina', precio_compra: 2.5, marca: 'Maizena', cantidad_disponible: 20, unidad_medida: "Kg" },
@@ -20,6 +31,17 @@ export class InventoryComponent {
     { id: 3, name: 'Aceite', precio_compra: 3.0, marca: 'Girasol', cantidad_disponible: 10, unidad_medida: 'lt' },
     { id: 4, name: 'Leche', precio_compra: 1.2, marca: 'Colanta', cantidad_disponible: 30, unidad_medida: 'lt' }
   ];
+
+
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      marca: ['', Validators.required],
+      precio_compra: [0, [Validators.required, Validators.min(0.1)]],
+      cantidad_disponible: [0, [Validators.required, Validators.min(1)]],
+      unidad_medida: ['', Validators.required]
+    });
+  }
 
   get filteredProducts() {
     return this.products.filter(product =>
@@ -30,29 +52,62 @@ export class InventoryComponent {
   onSearch() {
     console.log(`Buscando: ${this.searchTerm}`);
   }
-
+  //Open a form to register the info
   onAddProduct() {
-    console.log('Agregar producto');
-    // Aquí puedes abrir un diálogo o formulario para agregar un producto
+    this.showModal = true;
+    this.productForm.reset();
+  }
+
+  // Agrega el producto a la lista y cierra el modal
+  addProduct() {
+    if (this.productForm.valid) {
+      this.products.push(this.productForm.value);
+      this.showModal = false;
+    }
   }
 
   onRowSelected(product: any) {
     this.selectedProduct = product;
     console.log('Producto seleccionado:', product);
-  }
-
-  onEditProduct() {
+  } 
+//-------EDIT MODAL--------------  
+  openEditModal() {
     if (this.selectedProduct) {
-      console.log('Editar producto:', this.selectedProduct);
-      // Lógica para editar el producto seleccionado
+      this.editProductData = { ...this.selectedProduct };
+      this.showEditModal = true;
     }
   }
-
-  onDeleteProduct() {
+  saveEditedProduct() {
     if (this.selectedProduct) {
-      console.log('Eliminar producto:', this.selectedProduct);
-      this.products = this.products.filter(product => product.id !== this.selectedProduct.id);
+      const index = this.products.findIndex(p => p.id === this.selectedProduct.id);
+      if (index !== -1) {
+        this.products[index] = { ...this.products[index], ...this.editProductData };
+      }
+      this.showEditModal = false;
       this.selectedProduct = null;
     }
   }
+
+  closeEditModal() {
+    this.showEditModal = false;
+  }
+//---------
+  openDeleteModal() {
+    if (this.selectedProduct) {
+      this.showDeleteModal = true;
+    }
+  }
+
+  confirmDelete() {
+    if (this.selectedProduct) {
+      this.products = this.products.filter(product => product.id !== this.selectedProduct.id);
+      this.selectedProduct = null;
+      this.showDeleteModal = false;
+    }
+  }
+
+  closeDeleteModal() {
+    this.showDeleteModal = false;
+  }
+
 }
